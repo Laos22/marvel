109e9df8fc35c30cb312bfc33e3ec84295b68dbc
Laos22 <olega1987@gmail.com>
2024-11-12 14:54:36 +0200

    charInfo

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -112,4 +112,15 @@ const View = ({char}) => {
                         </p>
                         <div className="randomchar__btns">
                             <a href={homepage} className="button button__main">
-        
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+    )
+}
+
+export default RandomChar;
