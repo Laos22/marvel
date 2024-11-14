ec9758b5b501bcb30bd98f61f6e2a9deda48f223
Laos22 <olega1987@gmail.com>
2024-11-14 11:56:38 +0200

    change charinfo

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,13 +1,12 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import MarvelServices from '../../services/MarvelServices';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
-import Sceleton from '../skeleton/Skeleton'
+import Sceleton from '../skeleton/Skeleton';
 
 import './charInfo.scss';
-
 
 class CharInfo extends Component {
     constructor(props) {
@@ -16,16 +15,14 @@ class CharInfo extends Component {
             char: null,
             loading: false,
             error: false,
-        }
+        };
     }
-
-    
 
     componentDidMount() {
         // this.updateChar();
     }
 
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate(prevProps) {
         if (this.props.charId !== prevProps.charId) {
             this.updateChar();
         }
@@ -34,19 +31,18 @@ class CharInfo extends Component {
     componentWillUnmount() {
     }
 
-
     marvelServices = new MarvelServices();
 
     onCharLoaded = (char) => {
-        this.setState({char, loading: false});
+        this.setState({ char, loading: false });
     }
 
     onError = () => {
-        this.setState({error: true, loading: false})
+        this.setState({ error: true, loading: false });
     }
 
-    onLoading =() => {
-        this.setState({loading: true})
+    onLoading = () => {
+        this.setState({ loading: true });
     }
 
     updateChar = () => {
@@ -63,12 +59,12 @@ class CharInfo extends Component {
     }
 
     render() {
-        const {char, error, loading} = this.state;
+        const { char, error, loading } = this.state;
 
-        const sceleton = char || loading || error ? null : <Sceleton/>;
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error || sceleton) ? <View char={char}/>: null;
+        const sceleton = char || loading || error ? null : <Sceleton />;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error || sceleton) ? <View char={char} /> : null;
 
         return (
             <div className="char__info">
@@ -77,61 +73,72 @@ class CharInfo extends Component {
                 {spinner}
                 {content}
             </div>
-        )
+        );
     }
-
 }
 
-const View = ({char}) => {
-
-    const {name, thumbnail, description, homepage, wiki, comics} = char;
-    let style = {objectFit: 'cover'};
-    if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
-        style = {objectFit: 'unset'}
+class View extends Component {
+    constructor(props) {
+        super(props);
+        this.ref = React.createRef(); // Создаем реф для кнопки
     }
 
-    return(
-        <>
-            <div className="char__basics">
-                <img src={thumbnail} alt="abyss" style={style}/>
-                <div>
-                    <div className="char__info-name">{name}</div>
-                    <div className="char__btns">
-                        <a href={homepage} className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href={wiki} className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
+    componentDidUpdate(prevProps) {
+        // Устанавливаем фокус на кнопку при обновлении
+        if (this.props.char !== prevProps.char) {
+            this.ref.current.focus();
+        }
+    }
+
+    render() {
+        const { char } = this.props;
+        const { name, thumbnail, description, homepage, wiki, comics } = char;
+        let style = { objectFit: 'cover' };
+        if (thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
+            style = { objectFit: 'unset' };
+        }
+
+        return (
+            <>
+                <div className="char__basics">
+                    <img src={thumbnail} alt="abyss" style={style} />
+                    <div>
+                        <div className="char__info-name">{name}</div>
+                        <div className="char__btns">
+                            <a href={homepage} className="button button__main" ref={this.ref}> {/* Применяем реф к кнопке */}
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="char__descr">
-                {description}
-            </div>
-            <div className="char__comics">Comics:</div>
-            <ul className="char__comics-list">
-                {comics.length > 0 ? null: "Not comics"}
-                {
-                    comics.map((item, i) => {
-                        // eslint-disable-next-line
-                        if (i > 9) return;
-                        return (
-                            <li key={i} className="char__comics-item">
-                                {item.name}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </>
-    )
-
+                <div className="char__descr">
+                    {description}
+                </div>
+                <div className="char__comics">Comics:</div>
+                <ul className="char__comics-list">
+                    {comics.length > 0 ? null : "Not comics"}
+                    {
+                        comics.map((item, i) => {
+                            // eslint-disable-next-line
+                            if (i > 9) return;
+                            return (
+                                <li key={i} className="char__comics-item" tabIndex={0}>
+                                    {item.name}
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </>
+        );
+    }
 }
 
 CharInfo.propTypes = {
     charId: PropTypes.number
-
 }
 
 export default CharInfo;
