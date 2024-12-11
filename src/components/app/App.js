11243711e0530250654fd7d595dd3f29c3016d62
Laos22 <olega1987@gmail.com>
2024-12-11 21:54:04 +0200

    test

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,6 +8,7 @@ import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
+
 
 const  App = () => {
     const [currentChar, setCurrentChar] = useState(null);
